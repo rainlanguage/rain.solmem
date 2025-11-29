a1804d7515a73d61024c6558155808e052b6783e
thedavidmeister <thedavidmeister@gmail.com>
2025-11-29 20:02:33 +0400

    lint docs

--- src/lib/LibMemCpy.sol
+++ src/lib/LibMemCpy.sol
@@ -8,14 +8,10 @@ import {Pointer, LibPointer} from "./LibPointer.sol";
 
 library LibMemCpy {
     /// Copy an arbitrary number of bytes from one location in memory to another.
-    /// As we can only read/write bytes in 32 byte chunks we first have to loop
-    /// over 32 byte values to copy then handle any unaligned remaining data. The
-    /// remaining data will be appropriately masked with the existing data in the
-    /// final chunk so as to not write past the desired length. Note that the
-    /// final unaligned write will be more gas intensive than the prior aligned
-    /// writes. The writes are completely unsafe, the caller MUST ensure that
-    /// sufficient memory is allocated and reading/writing the requested number
-    /// of bytes from/to the requested locations WILL NOT corrupt memory in the
+    /// As of cancun this is a convenience wrapper for the `mcopy` opcode. The
+    /// writes are completely unsafe, the caller MUST ensure that sufficient
+    /// memory is allocated and reading/writing the requested number of bytes
+    /// from/to the requested locations WILL NOT corrupt memory in the
     /// opinion of solidity or other subsequent read/write operations.
     /// @param sourceCursor The starting pointer to read from.
     /// @param targetCursor The starting pointer to write to.
